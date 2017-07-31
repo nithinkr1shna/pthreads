ba948ed801fd267634957da2c65f5f3e04ecc204
nitkna <nithin.krishna@outlook.com>
2017-07-31 15:25:58 +0530

    dining philosophers problem with mutex locks

--- prg5.c
+++ prg5.c
@@ -13,26 +13,28 @@ void *fun(void*);
 int main(){
 
   int i;
-  for(i=0;i<5;i++)
+  for(i=0; i<5; i++)
     pthread_mutex_init(&chopstick[i],NULL);
-  for(i=0;i<5;i++)
+  for(i=0; i<5; i++)
     pthread_create(&philosopher[i],NULL, fun, (void*)&i);
   for(i=0; i<5; i++)
     pthread_join(philosopher[i],NULL);
+  for(i=0; i<5; i++)
+    pthread_mutex_destroy(&chopstick[i]);
    
 }
 
 void *fun(void *n){
 
   int num = *(int*)n;
-  printf(RED "philosopher %d is thinking\n" RESET, num);
+  printf(RED "philosopher %d is thinking\n" RESET, num+1);
   pthread_mutex_lock(&chopstick[num]);
   pthread_mutex_lock(&chopstick[(num+1)%5]);
-  printf(GREEN "philosopher %d is eating \n\n" RESET,num);
+  printf(GREEN "philosopher %d is eating using chopstick %d and %d \n\n" RESET,num+1,num+1, (num+1)%5+1);
   sleep(5);
   pthread_mutex_unlock(&chopstick[num]);
   pthread_mutex_unlock(&chopstick[(num+1)%5]);
-  printf(CYAN "Philosopher %d finished eating\n" RESET,num);
+  printf(CYAN "Philosopher %d finished eating and freed %d and %d\n" RESET,num+1, num+1 ,(num+1)%5+1);
 
   
 }
